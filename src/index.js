3ec7db5c1658e4bde3c9c021739bf5373e578d69
caduHD4 <cadumarianogarciapereira@gmail.com>
2026-04-06 22:07:54 -0300

    refatorar: reorganizar a inicialização do addon e adicionar configuração de serviço no render.yaml

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-const { serveHTTP } = require("stremio-addon-sdk");
-const { createAddonInterface } = require("./addonFactory");
+const { addonBuilder, serveHTTP } = require("stremio-addon-sdk");
+const manifest = require("./manifest");
+const { Q1NScraper } = require("./scraper/q1nScraper");
+const { buildCatalogHandler } = require("./handlers/catalog");
+const { buildMetaHandler } = require("./handlers/meta");
+const { buildStreamHandler } = require("./handlers/stream");
 
 async function bootstrap() {
-  const addonInterface = await createAddonInterface();
+  const scraper = new Q1NScraper();
+  const builder = new addonBuilder(manifest);
+
+  const catalogHandler = await buildCatalogHandler(scraper);
+  const metaHandler = await buildMetaHandler(scraper);
+  const streamHandler = await buildStreamHandler(scraper);
+
+  builder.defineCatalogHandler(catalogHandler);
+  builder.defineMetaHandler(metaHandler);
+  builder.defineStreamHandler(streamHandler);
+
   const port = Number(process.env.PORT || 7000);
-  serveHTTP(addonInterface, { port });
+  serveHTTP(builder.getInterface(), { port });
 
   console.log(`TopAnimes addon ativo em http://127.0.0.1:${port}/manifest.json`);
 }
