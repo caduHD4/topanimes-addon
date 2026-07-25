de383ec2d75f9be4e103d094105e4bc5792eb165
caduHD4 <cadumarianogarciapereira@gmail.com>
2026-07-24 21:51:20 -0300

    fix(filemoon): integrar extracao estatica e fallback de dominios espelhos no filemoon

--- src/extractors/fileMoonExtractor.js
+++ src/extractors/fileMoonExtractor.js
@@ -1,10 +1,36 @@
 const http = require("../utils/http");
 const { collectVideoUrls, asStreams } = require("./_shared");
+const { extractUniversal } = require("./universalExtractor");
 
 async function extractFileMoon(url) {
-  const response = await http.get(url);
-  const urls = collectVideoUrls(response.data, url);
-  return asStreams(urls, "FileMoon", url);
+  try {
+    const codeMatch = url.match(/\/e\/([a-zA-Z0-9]+)/);
+    const mirrors = [url];
+
+    if (codeMatch && codeMatch[1]) {
+      const code = codeMatch[1];
+      const backupDomains = ["filemoon.top", "filemoon.to", "filemoon.lat", "filemoon.nl"];
+      for (const d of backupDomains) {
+        const mirrorUrl = `https://${d}/e/${code}`;
+        if (mirrorUrl !== url) {
+          mirrors.push(mirrorUrl);
+        }
+      }
+    }
+
+    for (const targetUrl of mirrors) {
+      try {
+        const streams = await extractUniversal(targetUrl, "FileMoon");
+        if (streams && streams.length > 0) {
+          return streams;
+        }
+      } catch (_) {}
+    }
+
+    return [];
+  } catch (error) {
+    return [];
+  }
 }
 
 module.exports = {
